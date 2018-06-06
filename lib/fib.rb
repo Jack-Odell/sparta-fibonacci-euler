b67749e55a29d0e0197bbd68bb36580e52980c1f
Jack Odell <jackodellmusic@gmail.com>
2018-06-06 22:36:37 +0100

    Test for fibonacci and even numbers in fibonacci passed

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,3 +1,28 @@
 class FibNumber
-  
+
+  attr_accessor :fib_arr
+  attr_accessor :even_fib_arr
+
+  def initialize
+    @fib_arr = []
+    @even_fib_arr = []
+  end
+
+  def the_fib(n)
+    @fib_arr[n] =
+    if n <= 1
+      n
+    else
+      @fib_arr[n] ||= the_fib(n-1) + the_fib(n-2)
+    end
+  end
+
+  def even_fib
+    @fib_arr.each {
+      |x|
+      if x % 2 == 0
+        @even_fib_arr << x
+      end
+      }
+  end
 end
--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -1,7 +1,15 @@
 require 'fib'
 
 describe FibNumber do
-  before(:all) do
+  before(:each) do
     @fib = FibNumber.new
+  end
+
+  it 'Should return 55 at 10th position of fibonacci' do
+    expect(@fib.the_fib(10)).to eq(55)
+  end
+
+  it 'Should only contain even numbers' do
+    expect(@fib.even_fib_arr).to all(be_even)
   end
 end
